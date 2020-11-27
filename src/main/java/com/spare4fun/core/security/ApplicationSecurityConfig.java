8c0aaa47f2d3f145e1e085d1e499923e87466189
Alicewillbe <zhaolonglong4008@gmail.com>
2020-11-26 20:42:13 -0800

    Update ApplicationSecurityConfig.java
    just like onlineShop

--- src/main/java/com/spare4fun/core/security/ApplicationSecurityConfig.java
+++ src/main/java/com/spare4fun/core/security/ApplicationSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.ServletException;
@@ -38,6 +39,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                    //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                     .csrf().disable()
                     .authorizeRequests()
                     .antMatchers("/index", "/login", "/logout").permitAll()
